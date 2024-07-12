c74054e9414d594e77f016a879a76e05850b252d
shruti100925 <120240503+shruti100925@users.noreply.github.com>
2024-07-12 15:43:04 +0530

    commit

---
+++ module2/1_printinfo.dart
@@ -0,0 +1,9 @@
+
+void main()
+{
+  print("Shruti Gadhavi");
+  print("25/10/1993");
+  print("30");
+  print("visamo,kailashdhara park soc-2,raiya road");
+
+}
---
+++ module2/2_arithmeticoperations.dart
@@ -0,0 +1,19 @@
+void main()
+{
+  var a=5;
+  var b=10;
+
+  var add=a + b;
+  print("$add");
+
+  var mul=a * b;
+  print("$mul");
+
+  var sub= b - a;
+  print("$sub");
+
+  var div=a / b;
+  print("$div");
+
+
+}
