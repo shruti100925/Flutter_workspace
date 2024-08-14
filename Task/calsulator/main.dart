eb7151f1ff9eff249b83d5b52d11a7f8ccb11aba
shruti100925 <120240503+shruti100925@users.noreply.github.com>
2024-08-14 14:15:49 +0530

    Add files via upload

---
+++ Task/calsulator/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertask/firstscreen.dart';
+
+void main()
+{
+  runApp
+    (
+      MaterialApp
+        (
+          home: firstscreen()
+      )
+  );
+}
+
+
+
